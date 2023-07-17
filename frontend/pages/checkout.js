13c12443d4ddc756b69657377861bfdf0baccc4a
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-07-17 12:41:02 -0700

    Create checkout.js

---
+++ frontend/pages/checkout.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { Row, Col } from "reactstrap";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "@/components/CheckoutForm";
+import AppContext from "../components/AppContext";
+import Cart from "../components/cart";
+
+function Checkout() {
+  const { isAuthenticated } = useContext(AppContext);
+
+  const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK);
+
+  return (
+    <Row>
+      <Col style={{ paddingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
+        <h1 style={{ margin: 20 }}>Checkout</h1>
+        <Cart isAuthenticated={isAuthenticated} />
+      </Col>
+      <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </Col>
+    </Row>
+  );
+}
+
+export default Checkout;
